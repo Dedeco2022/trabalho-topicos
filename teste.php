24f8cd7e12572946eabae54f525707bb276331a4
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-08-09 08:19:58 -0300

    Teste para realizar o login

--- teste.php
+++ teste.php
@@ -1,21 +1,21 @@
 <?php
 session_start();
-if (isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['senha'])) {
+if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
     require_once('conecta.php');
     $conexao= conectar();
-    $nome = $_POST['nome'];
+    $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM usuario WHERE nome = '$nome' AND senha = '$senha'";
+    $sql = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
     $resultado = mysqli_query($conexao, $sql);
 
     if (mysqli_num_rows($resultado) < 1) {
 
-        unset($_SESSION['nome']);
+        unset($_SESSION['email']);
         unset($_SESSION['senha']);
         header('Location: index?erro=1');
     } else {
-        $_SESSION['nome'] = $nome;
+        $_SESSION['email'] = $email;
         $_SESSION['senha'] = $senha;
         header('Location: pagina_inicial.php');
     }
